23f7ce846f5240f1617d487174559468d380f205
Benjamin Gaull <brgaull1998@hotmail.com>
2019-08-28 10:48:27 -0700

    added app_test to determine the format of extend partition

--- Lab 1/Part 1/Lab 1/app.c
+++ Lab 1/Part 1/Lab 1/app.c
@@ -6,24 +6,25 @@
 
 
 int main(int argc, char *argv[], char *env[]) {
-
+	//establish local variables
 	int fd, * mbr_addr, temp; 
 	char buf[512];
 	struct partition * table[4];
 
-
+	//open vdisk
 	fd = open("vdisk", O_RDONLY);  // check fd value: -1 means open() failed
-	if (fd != -1) {
+	if (fd != -1) {						//if open does not fail
 		read(fd, buf, 512);            // read sector 0 into buf[ ]
-		mbr_addr = &buf;
+		mbr_addr = &buf;				//get address of buf to use to read MBR
 
 		//read in partition table
-		for (temp = 0; temp < 4; ++temp) {
-			table[temp] = mbr_addr + (temp * PART_TABLE_OFFSET);
+		for (temp = 0; temp < 4; ++temp) {		//read partitions into struct partition table for easy reading
+			table[temp] = mbr_addr + PART_TABLE_OFFSET + (temp * sizeof(struct partition));
 		}
 
 		//print the partition table
 		for (temp = 0; temp < 4; ++temp) {
+			//test if partition type is Extend
 			if (table[temp]->sys_type != 5 && table[temp]->sys_type != '5') {
 				printf("Partition %d\n", (temp + 1));	//debug print 
 
@@ -48,8 +49,8 @@ int main(int argc, char *argv[], char *env[]) {
 				//print nr_sectors;
 				printf("Nr_Sectors: %u", table[temp]->nr_sectors);
 			}
-			else {
-				printf("Partition %d\n", (temp + 1));	//debug print 
+			else {	//type is Extend
+				//printf("Partition %d\n", (temp + 1));	//debug print 
 
 				//print drive;
 				printf("Drive: %c", table[temp]->drive);
@@ -72,8 +73,21 @@ int main(int argc, char *argv[], char *env[]) {
 				//print nr_sectors;
 				printf("Nr_Sectors: %u", table[temp]->nr_sectors);
 				
-				//print EXTEND Partition
 
+				//print EXTEND Partition
+				int extend_start = table[temp]->start_sector;		//get location of first local MBR
+				lseek(fd, (long)extend_start * 512, SEEK_SET);		//find in local MBR
+				read(fd, buf, 512);									//load in local MBR
+
+				//load extend partition table
+
+
+
+
+
+
+
+				//
 
 
 			}
