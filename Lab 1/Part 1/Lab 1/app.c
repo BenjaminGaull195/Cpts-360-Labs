95a44383b8ff7095239b2c215e858e2d91a09932
Benjamin Gaull <brgaull1998@hotmail.com>
2019-08-29 11:58:58 -0700

    fux

--- Lab 1/Part 1/Lab 1/app.c
+++ Lab 1/Part 1/Lab 1/app.c
@@ -74,21 +74,47 @@ int main(int argc, char *argv[], char *env[]) {
 				printf("Nr_Sectors: %u", table[temp]->nr_sectors);
 				
 
+
 				//print EXTEND Partition
 				int extend_start = table[temp]->start_sector;		//get location of first local MBR
-				lseek(fd, (long)extend_start * 512, SEEK_SET);		//find in local MBR
-				read(fd, buf, 512);									//load in local MBR
+				struct partition * local_par;
+				int temp2 = 0;
 
-				//load extend partition table
+				while (extend_start != 0) {
+					lseek(fd, (long)extend_start * 512, SEEK_SET);		//find in local MBR
+					read(fd, buf, 512);									//load in local MBR
+
+					//load extend partition table
+					local_par = mbr_addr + PART_TABLE_OFFSET + (temp2 * sizeof(struct partition));
 
 
+					//print partition
 
+					//print drive;
+					printf("Drive: %c", local_par->drive);
+					//print head;
+					printf("Head: %c", local_par->head);
+					//print sector;
+					printf("Sector: %c", local_par->sector);
+					//print cylinder;
+					printf("Cylinder: %c", local_par->cylinder);
+					//print sys_type;
+					printf("Sys_type: %c", local_par->sys_type);
+					//print end_head;
+					printf("End_Head: %c", local_par->end_head);
+					//print end_sector;
+					printf("End_Sector: %c", local_par->end_sector);
+					//print end_cylinder;
+					printf("End_Cylinder: %c", local_par->end_cylinder);
+					//print start_sector;
+					printf("Start_Sector: %u", local_par->start_sector);
+					//print nr_sectors;
+					printf("Nr_Sectors: %u", local_par->nr_sectors);
 
+					//int extend_start = table[temp]->start_sector;		//get location of local MBR
+					++temp2;
 
-
-
-				//
-
+				}
 
 			}
 		}
