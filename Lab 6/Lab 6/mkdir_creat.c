ce9ce3ea795259213e1bb78828d49cfe2e20d09b
Benjamin Gaull <brgaull1998@hotmail.com>
2019-11-14 13:50:31 -0800

    fix

--- Lab 6/Lab 6/mkdir_creat.c
+++ Lab 6/Lab 6/mkdir_creat.c
@@ -22,8 +22,8 @@ extern MINODE minode[NMINODE];
 extern MINODE *root;
 extern PROC   proc[NPROC], *running;
 
-char *t1 = "xwrxwrxwr-------";
-char *t2 = "----------------";
+//char *t1 = "xwrxwrxwr-------";
+//char *t2 = "----------------";
 
 
 
